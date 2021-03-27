6c42a008f0c5bcfcea6af5b4eb4b2ac56558835a
Karol011 <karollacek1@gmail.com>
2021-03-27 20:55:07 +0100

    cleaned up main method

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,27 +10,5 @@ import org.hibernate.Transaction;
 public class Main {
     public static void main(String[] args) {
 
-
-        PeopleClient peopleClient = new PeopleClient();
-        PeopleDto peopleDto = peopleClient.getAstronautsForADate();
-        PeopleDao peopleDao = new PeopleDao();
-        peopleDao.saveAll(peopleDto.getPeople());
-
-        Station station = new Station();
-        station.setId(1);
-        station.setName("namaaaaeee");
-
-
-        Transaction transaction = null;
-
-        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
-            transaction = session.beginTransaction();
-            session.saveOrUpdate(station);
-            transaction.commit();
-        } catch (HibernateException hibernateException) {
-            transaction.rollback();
-        }
-
     }
-
 }
