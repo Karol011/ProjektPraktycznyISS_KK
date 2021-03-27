3d9bf861bdbb4c1d9e61b92f6a0c6fb73063f0ed
Karol011 <karollacek1@gmail.com>
2021-03-27 20:53:04 +0100

    app is now using service layer for getting people that are currently in ISS

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,31 +6,12 @@ import external.entity.Station;
 import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
+import service.PeopleService;
 
 public class Main {
     public static void main(String[] args) {
-
-
-        PeopleClient peopleClient = new PeopleClient();
-        PeopleDto peopleDto = peopleClient.getAstronautsForADate();
-        PeopleDao peopleDao = new PeopleDao();
-        peopleDao.saveAll(peopleDto.getPeople());
-
-        Station station = new Station();
-        station.setId(1);
-        station.setName("namaaaaeee");
-
-
-        Transaction transaction = null;
-
-        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
-            transaction = session.beginTransaction();
-            session.saveOrUpdate(station);
-            transaction.commit();
-        } catch (HibernateException hibernateException) {
-            transaction.rollback();
-        }
-
+        PeopleService peopleService = new PeopleService();
+        peopleService.getCurrentPeopleInSpace().forEach(System.out::println);
     }
 
 }
--- src/main/java/external/entity/People.java
+++ src/main/java/external/entity/People.java
@@ -27,5 +27,12 @@ public class People {
     @Cascade(org.hibernate.annotations.CascadeType.PERSIST)
     private Station station;
 
-
+    @Override
+    public String toString() {
+        return "People{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", station=" + station +
+                '}';
+    }
 }
