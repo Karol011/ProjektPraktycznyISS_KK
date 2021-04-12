8da2c951773ad0e77b21e572419b07221494fb28
Karol011 <karollacek1@gmail.com>
2021-04-12 21:29:15 +0200

    adjusted menu options

--- src/main/java/view/Menu.java
+++ src/main/java/view/Menu.java
@@ -13,23 +13,22 @@ public class Menu {
 
     public static void main(String[] args) {
 
-
         greetUser();
 
     }
 
     public static void greetUser() {
-        System.out.println("Witaj w projekcie prezentujacym wybrane dane i statystyki na podstawie open-notify.org API\n" +
-                " wybierz operacje:\n" +
-                "1. oblicz predkosc ISS\n" +
-                "2. Zwracanie listy nadchodzących przebiegów ISS dla określonej lokalizacji\n" +
-                "3. Zwracanie liczby osób przebywających w kosmosie w ramach ISS\n" +
-                "4. Zwracanie średniej prędkości w określonym fragmencie czasu, np. miesiąc, rok\n" +
-                "5. ile razy ISS znajdowało się nad daną lokalizacją w przedziale czasu\n" +
-                "6. liczbę osób przebywających w ramach misji kosmicznej na ISS\n" +
-                "7. obecne koordynaty stacji kosmicznej\n" +
-                "0. wyjscie z aplikacji\n");
-        displayMenu();
+            System.out.println("Witaj w projekcie prezentujacym wybrane dane i statystyki na podstawie open-notify.org API\n" +
+                    " wybierz operacje:\n" +
+                    "1. obecne koordynaty stacji kosmicznej\n" +
+                    "2. oblicz predkosc ISS\n" +
+                    "3. Zwracanie średniej prędkości w określonym fragmencie czasu, np. miesiąc, rok\n" +
+                    "4. Zwracanie liczby osób przebywających w kosmosie w ramach ISS\n" +
+                    "5. ile razy ISS znajdowało się nad daną lokalizacją w przedziale czasu\n" +
+                    "0. wyjscie z aplikacji\n");
+            displayMenu();
+
+
     }
 
     private static void displayMenu() {
@@ -40,27 +39,24 @@ public class Menu {
             greetUser();
         }
         switch (choice) {
+
             case '1':
-                System.out.printf("Srednia predkosc iss %.2f metrów na sekundę",stationPositionService.getStationSpeed());
+                System.out.println(stationPositionService.getCurrentPosition());
                 break;
             case '2':
-                System.out.println("W trakcie tworzenia...");
+                System.out.printf("Srednia predkosc iss %.2f metrów na sekundę", stationPositionService.getStationSpeed());
                 break;
             case '3':
-                peopleService.getCurrentPeopleInSpace().forEach(System.out::println);
+                System.out.println("W trakcie tworzenia...");
                 break;
             case '4':
-                System.out.println("W trakcie tworzenia...");
+                peopleService.getCurrentPeopleInSpace().forEach(System.out::println);
                 break;
             case '5':
                 System.out.println("W trakcie tworzenia...");
                 break;
-            case '6':
-                System.out.println("W trakcie tworzenia...");
-                break;
-            case '7':
-                System.out.println(stationPositionService.getCurrentPosition());
-                break;
+
+
             case '0':
 
 
